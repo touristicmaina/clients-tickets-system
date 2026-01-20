5af52a41b71ce22fefb741fb26744a1988f095a8
touristicmaina <touristicmaina@gmail.com>
2026-01-21 01:46:23 +0300

    Update app.routes.ts

--- client-ticket-app/src/app/app.routes.ts
+++ client-ticket-app/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login';
+import { AuthGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthGuard],
+    loadComponent: () =>
+      import('./app').then(m => m.AppComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
