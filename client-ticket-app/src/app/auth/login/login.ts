2c586dd3ae9e3b546dfa7f2580403feeb48f1d63
touristicmaina <touristicmaina@gmail.com>
2026-01-21 01:55:00 +0300

    Update login.ts

--- client-ticket-app/src/app/auth/login/login.ts
+++ client-ticket-app/src/app/auth/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss']
 })
-export class Login {
+export class LoginComponent {
+  email = '';
+  password = '';
+  error = '';
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  login() {
+    const success = this.authService.login(this.email, this.password);
+
+    if (success) {
+      this.router.navigate(['/']);
+    } else {
+      this.error = 'Invalid email or password';
+    }
+  }
 }
