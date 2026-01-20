7dc56e6eb638ec1973a33bfaeb80b59f9ee14e91
touristicmaina <touristicmaina@gmail.com>
2026-01-21 01:57:03 +0300

    Update auth.ts

--- client-ticket-app/src/app/core/services/auth.ts
+++ client-ticket-app/src/app/core/services/auth.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Auth {
-  
+export class AuthService {
+  private loggedIn = false;
+  private role: 'admin' | 'user' = 'user';
+
+  login(email: string, password: string): boolean {
+    if (email === 'admin@test.com' && password === '1234') {
+      this.loggedIn = true;
+      this.role = 'admin';
+      return true;
+    }
+
+    if (email === 'user@test.com' && password === '1234') {
+      this.loggedIn = true;
+      this.role = 'user';
+      return true;
+    }
+
+    return false;
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+  getRole(): string {
+    return this.role;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
 }
