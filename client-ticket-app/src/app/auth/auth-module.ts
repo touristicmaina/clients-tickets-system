339f6ad04d3839a529952ebb5c4f4cf1ee2ba9cf
touristicmaina <touristicmaina@gmail.com>
2026-01-21 01:54:22 +0300

    Update auth-module.ts

--- client-ticket-app/src/app/auth/auth-module.ts
+++ client-ticket-app/src/app/auth/auth-module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing-module';
-
+import { LoginComponent } from './login/login';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     AuthRoutingModule
   ]
 })
-export class AuthModule { }
+export class AuthModule {}
